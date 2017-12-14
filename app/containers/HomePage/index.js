f26c9293037e295922e86fdb825e78dc79028f6e
Emre Avci <emre.avci@flixbus.com>
2017-12-14 01:07:43 +0100

    Added draft js and first editor

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,14 +11,45 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 import messages from './messages';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  constructor(props) {
+    super(props);
+    this.state = { editorState: EditorState.createEmpty() };
+    this.onChange = (editorState) => this.setState({ editorState });
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+
+  onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+
+  onH1Click() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'H1'));
+  }
+
+  handleKeyCommand(command, editorState) {
+    const newState = RichUtils.handleKeyCommand(editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
+  }
+
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+        <button onClick={this.onBoldClick.bind(this)}>Bold</button>
+        <button onClick={this.onH1Click.bind(this)}>H1</button>
+        <Editor
+          editorState={this.state.editorState}
+          onChange={this.onChange}
+          handleKeyCommand={this.handleKeyCommand}
+        />
+      </div>
     );
   }
 }
